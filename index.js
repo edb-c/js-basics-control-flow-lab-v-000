f13587875d4e849316b823f9d97137dc15bd84bc
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 12:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   else  (someValue > 2000)
     return 'I will gladly take your thirty bucks.'
   else if (someValue => 2500)
