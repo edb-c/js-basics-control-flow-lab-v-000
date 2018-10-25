cbbf5d75dc4dc2530bda578386b7c8a3dcd94369
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 12:12:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// Write your code in this file!
+//gives customers a free sample if the ride is less than or equal to 400 feet ‣
+AssertionError: expected undefined to equal 'This one is on me!'
+    at Context.<anonymous> (test/indexTest.js:6:45)
+charges 30 dollars for a distance over 2000 feet ‣
+AssertionError: expected undefined to equal 'I will gladly take your thirty bucks.'
+    at Context.<anonymous> (test/indexTest.js:10:46)
+does not allow rides over 2500 feet ‣
+AssertionError: expected undefined to equal 'No can do.'
+    at Context.<anonymous> (test/indexTest.js:14:46)
 
 
 function scuberGreetingForFeet(someValue) {
-  //this is where we can use conditionals given our argument, someValue
-  //don't forget to return whatever the result is!
+  if someValue <= 400
+    return 'This one is on me!'
+  else if someValue > 2000
+    return 'I will gladly take your thirty bucks.'
+  else if someValue => 2500
+    return 'No can do.'
+  end;  
 }
 
 function ternaryCheckCity( ){
