c60796a503af401c2e37c6da38a1ebacb655cf94
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 12:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     return 'This one is on me!';
   }
-  else if (someValue > 2000 && ) {
+  else if (someValue > 2000 &&  someValue <= 2499) {
     return 'I will gladly take your thirty bucks.';
   }
-  else if (someValue > 2500) {
+  else if (someValue => 2500) {
     return 'No can do.';
   }
 }
