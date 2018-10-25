78e3f762c1cac9b65645cfbdae4a519be24b285d
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 12:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function scuberGreetingForFeet(someValue) {
 
 
 function ternaryCheckCity(someCity ){
-  return ("NYC" ? 'Ok, sounds good.' : "$10.00");
+  return ("NYC" ? 'Ok, sounds good.' : 'No go.');
 }
 
 function switchOnCharmFromTip() {
