8909092abc8c9f912f7aaeccea54e7c34af94887
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 12:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //gives customers a free sample if the ride is less than or equal to 400 feet ‣
 
-
+//scuberGreetingForFeet()
+//gives customers a free sample if the ride is less than or equal to 400 feet ‣
+//charges 30 dollars for a distance over 2000 feet ‣
+//does not allow rides over 2500 feet
 
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
